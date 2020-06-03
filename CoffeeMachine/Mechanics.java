5d903a0843115708bc6ace73871a1933c24c665a
Dominik Zajączkowski <dzajaczkowski261@gmail.com>
2020-06-03 03:32:13 +0200

    Folder name changes

--- CoffeeMachine/Mechanics.java
+++ CoffeeMachine/Mechanics.java
@@ -1,20 +1,5 @@
 package machine;
 
-import java.util.Scanner;
-
-public class CoffeeMachine {
-
-    public static void main(String[] args) {
-        System.out.println("Write action (buy, fill, take, remaining, exit):");
-        Mechanics mechanics = new Mechanics();
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            mechanics.processInputCommands(sc.next());
-        }
-    }
-}
-
-
 class Mechanics {
 
     private int water;
